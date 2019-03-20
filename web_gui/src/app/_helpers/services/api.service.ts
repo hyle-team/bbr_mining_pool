d589e402b497032a0c1e60b4655cc5dc77ab7349
wildkif <kif@zano.org>
2019-03-20 11:13:57 +0200

    replace and show new data from new api response

--- web_gui/src/app/_helpers/services/api.service.ts
+++ web_gui/src/app/_helpers/services/api.service.ts
@@ -7,7 +7,7 @@ import {map} from 'rxjs/operators';
   providedIn: 'root'
 })
 export class ApiService {
-  private serverApi = location.protocol + '//' + location.hostname + ':3000';
+  private serverApi = 'http://' + location.hostname + ':3000';
 
   constructor(private http: HttpClient) {
   }
